693b07470a13f7184e55f88bc08bf8bd7320d8a8
fengxin19860616 <56420139+fengxin19860616@users.noreply.github.com>
2020-02-27 17:26:36 +0800

    注释增加

--- app/src/components/TheCard/TheCard.js
+++ app/src/components/TheCard/TheCard.js
@@ -2,18 +2,24 @@ import React, { useState } from 'react';
 import './TheCard.css';
 
 /**
- * `TheCard`函数组件传参处理
- * @description HOOK learn: https://zh-hans.reactjs.org/docs/hooks-intro.html
- * @param {any} props 
+ * `TheCard`函数组件
+ * @description 
+ * 函数组件有个好处就是代码注释可以跨文件提示，`calss`组件不行 <br />
+ * HOOK learn: https://zh-hans.reactjs.org/docs/hooks-intro.html
+ * @param {object} props 组件参数
+ * @param {string} props.title 标题
+ * @param {string} props.poster 图片
+ * @param {string} props.posterDec 图片描述
+ * @param {string} props.content 内容
+ * @param {boolean} props.options 是否需要按钮操作
+ * @param {(value: string) => void} props.setContent 传给父组件的回调函数
  */
 export default function TheCard(props) {
-
-    // console.log(props);
 
     /** 默认数据 */
     const defaultData = {
         title: 'The card title',
-        poster: 'https://muse-ui.org/img/sun.a646a52d.jpg',
+        poster: 'https://54d4a4629fb6b.t73.qifeiye.com/qfy-content/uploads/2015/06/27f237e6b7f96587b6202ff3607ad88a.jpg',
         posterDec: 'The poster',
         content: '散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！',
     }
@@ -28,6 +34,9 @@ export default function TheCard(props) {
     function setDefaultContent() {
         // console.log(props);
         props.setContent(defaultData.content);
+        // fetch('https://douban.uieee.com/v2/movie/top250?start=1&count=5').then(response => response.json()).then(res => {
+        //     console.log('fetch', res);
+        // })
     }
 
     return (
--- app/src/views/next-page/next-page.js
+++ app/src/views/next-page/next-page.js
@@ -11,12 +11,10 @@ export default function NextPage(props) {
         content: '起飞页是一款简单、便捷的工具，可以帮助您制作响应式网站。我们的网站构建器，将传统的编码工作转化为直观的拖拽操作和文字录入。在这里，您仅需花上10分钟便可以完成一个H5响应式网站，您会发现完全不需要编写一行代码，这就是起飞页自助建站的神奇之处。起飞页是一款简单、便捷的工具，可以帮助您制作响应式网站。我们的网站构建器，将传统的编码工作转化为直观的拖拽操作和文字录入。在这里，您仅需花上10分钟便可以完成一个H5响应式网站，您会发现完全不需要编写一行代码，这就是起飞页自助建站的神奇之处。'
     });
     
-    const cardTwo = {
-        title: 'Card Two',
-        poster: 'https://54d4a4629fb6b.t73.qifeiye.com/qfy-content/uploads/2015/06/c6bdf6f65f3845da9085e9ae5790b494.jpg',
-        posterDec: '森林-糜花鹿',
-        content: '起飞页是一款简单、便捷的工具，可以帮助您制作响应式网站。我们的网站构建器，将传统的编码工作转化为直观的拖拽操作和文字录入。在这里，您仅需花上10分钟便可以完成一个H5响应式网站，您会发现完全不需要编写一行代码，这就是起飞页自助建站的神奇之处。起飞页是一款简单、便捷的工具，可以帮助您制作响应式网站。我们的网站构建器，将传统的编码工作转化为直观的拖拽操作和文字录入。在这里，您仅需花上10分钟便可以完成一个H5响应式网站，您会发现完全不需要编写一行代码，这就是起飞页自助建站的神奇之处。'
-    }
+    const cardTwo = {...cardOne};
+    cardTwo.title = 'Card Two';
+
+    const propsData = `props参数: \n ${JSON.stringify(props, null, 4)}`;
 
     function modifyCardOneContent(value) {
         // console.log('modifyCardOneContent', value);
@@ -34,8 +32,8 @@ export default function NextPage(props) {
     return (
         <div className="next-page">
             <TheCard title={ cardOne.title } poster={ cardOne.poster } posterDec={ cardOne.posterDec } content={ cardOne.content } options={ true } setContent={ modifyCardOneContent } />
-            <TheCard title={ cardTwo.title } poster={ cardTwo.poster } posterDec={ cardTwo.posterDec } content={ cardTwo.content } />
-            <div style={{ marginBottom: '10px' }}>页面参数：{ JSON.stringify(props, null, 4) }</div>
+            <TheCard title="Card Two" posterDec="Horses and beauties" />
+            <textarea className="props-box" defaultValue={ propsData }></textarea>
             <button className="button green-btn" onClick={ goBack }>返回</button>
         </div>
     )
